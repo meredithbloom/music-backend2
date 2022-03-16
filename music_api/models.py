0a381223731a71bafef3e03d27a13826106797bd
Lily Chen <chenlily1998@gmail.com>
2022-03-15 23:06:55 -0400

    updated model

--- music_api/models.py
+++ music_api/models.py
@@ -4,6 +4,18 @@ from django.db import models
 class Song(models.Model):
     name = models.CharField(max_length = 100)
     artist = models.CharField(max_length = 100)
-    genre = models.CharField(max_length = 100)
+    genre_choices = [
+        ('pop', 'Pop'),
+        ('rock', 'Rock'),
+        ('techno', 'Techno'),
+        ('hiphop', 'Hip-hop'),
+        ('jazz', 'Jazz'),
+        ('rap', 'Rap'),
+        ('country', 'Country'),
+        ('metal', 'Metal'),
+        ('alternative', 'Alternative'),
+        ('indie', 'Indie'),
+    ]
+    genre = models.CharField(max_length = 100, choices=genre_choices, default='POP')
     audio = models.CharField(max_length = 100)
     image = models.CharField(max_length = 100)
