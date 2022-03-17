3189803066109e5aeba2b557b394b7d3869e514f
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-03-17 17:22:56 -0400

    fixed song serializer to match model

--- music_api/serializers.py
+++ music_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Song
 class SongSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Song # tell django which model to use
-        fields = ('id', 'name', 'artist', 'genre', 'audio', 'image', 'price', 'release_type') # tell django which fields to include
+        fields = ('id', 'name', 'artist', 'genre', 'audio', 'image', 'price') # tell django which fields to include
