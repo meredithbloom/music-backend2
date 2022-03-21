dbec04e57804ea10202ad1cab85cfba4374e17b8
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-03-21 15:49:02 -0400

    messing with user and account models so genre choices display as an array

--- user_auth_api/admin.py
+++ user_auth_api/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import UserAccount
-admin.site.register(UserAccount)
+from django.contrib.postgres.fields import ArrayField 
+
+
 
 # Register your models here.
+from .models import UserAccount
 from .models import Account
-admin.site.register(Account)
+
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['favorite_genres']
+
+
+admin.site.register(UserAccount)
+admin.site.register(Account, AccountAdmin)
+
+
+
