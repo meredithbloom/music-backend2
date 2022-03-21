de72a46d0b24af3a53094aeeb96e2aa96fef6dbf
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-03-21 16:40:02 -0400

    finally connected useraccount and account models, we are able to enter multiple strings in genre array

--- user_auth_api/admin.py
+++ user_auth_api/admin.py
@@ -8,12 +8,12 @@ from .models import UserAccount
 from .models import Account
 
 
-class AccountAdmin(admin.ModelAdmin):
-    list_display = ['favorite_genres']
+# class AccountAdmin(admin.ModelAdmin):
+#     list_display = ('favorite_genres',)
 
 
 admin.site.register(UserAccount)
-admin.site.register(Account, AccountAdmin)
+admin.site.register(Account)
 
 
 
