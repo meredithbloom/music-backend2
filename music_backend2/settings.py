6ac8a27b1b33fd9675d0b68afdfcdea4ac2f9107
Lily Chen <chenlily1998@gmail.com>
2022-03-21 11:02:12 -0400

    merging dev to master

--- music_backend2/settings.py
+++ music_backend2/settings.py
@@ -62,11 +62,10 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 SESSION_SAVE_EVERY_REQUEST = False
-<<<<<<< HEAD
-=======
+
 
 #session cookies will be stored in users' browsers for SESSION_COOKIE_AGE seconds, which defaults to two weeks (1,209,600 seconds. use this if you don't want users to have to log in every time they open a browser)
->>>>>>> 162ea03b3698c3e8dca045f01fc242639fdbaaed
+
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 
 ROOT_URLCONF = 'music_backend2.urls'
@@ -96,7 +95,7 @@ DJANGO_SETTINGS_MODULE = 'django-admin'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'music_backend2',
+        'NAME': 'music_api',
         'USER': '',
         'PASSWORD': '',
         'HOST': 'localhost'
